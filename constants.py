90bda30cf5ebaed6bc290b026d9f97e498cf5259
zant2f <foot@bell.net>
2020-09-19 22:39:58 -0400

    constants.py updated

--- constants.py
+++ constants.py
@@ -49,3 +49,27 @@ ITEM_END_TAG = '/item'
 EPHEMERAL_TAG = 'ephemeral'
 MERCHANT_TAG = 'merchant'
 COMBAT_TAG = 'combat'
+
+STATS_PER_LEVEL = {
+    1: (500, 0, 20, 200),
+    2: (600, 1, 25, 300),
+    3: (700, 2, 30, 500),
+    4: (700, 3, 40, 800),
+    5: (900, 4, 50, 1100),
+    6: (1000, 5, 60, 1400),
+    7: (1100, 6, 80, 1700),
+    8: (1200, 7, 100, 2000),
+    9: (1300, 8, 120, 2400),
+    10: (1400, 9, 140, 2800),
+}
+XP_GAIN = {
+    'Chauve_Souris': 50,
+    'FantomeNoir': 100,
+    'SoldatSquel': 120,
+    'OrbeBleue': 50,
+    'SoldatSombre': 400,
+    'Loup': 150,
+    'Squelette': 90,
+    'Diable': 150,
+    'Boss': 1000,
+}
