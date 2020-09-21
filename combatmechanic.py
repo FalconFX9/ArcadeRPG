ef867d01d69c1c7e06d1329e5465121040c77d4a
zant2f <foot@bell.net>
2020-09-20 20:33:07 -0400

    chest go brrrrrr

--- combatmechanic.py
+++ combatmechanic.py
@@ -50,7 +50,7 @@ class CombatMechanic:
 
     def calculate_XP(self):
         XP_gain = 0
-        for entity in self.state.combat_launch_entities.obtain_component(InitiateCombat).combat_entities.values():
+        for entity in self.state.combat_launch_entity.obtain_component(InitiateCombat).combat_entities.values():
             level = entity.obtient_composant(Stats).level
             XP_gain += C.XP_GAIN[entity.id] * (1 + (level / 5))
         return XP_gain
@@ -70,10 +70,10 @@ class CombatMechanic:
                 stats_joueur.XP += self.calculate_XP()
                 if stats_joueur.vérifie_niveau():
                     self.state.joueur.obtient_composant(Inventory).vérifie_stats()
-                self.state.entités_détruites.append(self.state.combat_launch_entities.id)
-                if 'Boss' in self.state.combat_launch_entities.id:
+                self.state.entités_détruites.append(self.state.combat_launch_entity.id)
+                if 'Boss' in self.state.combat_launch_entity.id:
                     self.state.state = C.GAME_END
-                self.state.entités.remove(self.state.combat_launch_entities)
+                self.state.entités.remove(self.state.combat_launch_entity)
                 self.state.joueur.obtient_composant(Controllable).force = None
                 self.flee = False
         elif self.state.bouttons['Flee'].cliqué:
@@ -139,8 +139,8 @@ class CombatMechanic:
             self.check_selected_enemy()
             self.player_turn()
         else:
-            enemies_num = len(self.state.combat_launch_entities.obtain_component(InitiateCombat).combat_entities.values())
-            if 'Boss' not in self.state.combat_launch_entities.id:
+            enemies_num = len(self.state.combat_launch_entity.obtain_component(InitiateCombat).combat_entities.values())
+            if 'Boss' not in self.state.combat_launch_entity.id:
                 if self.counter in self.enemies.keys():
                     self.enemy_turn(self.counter)
                 else:
--- tiles/Chesttile.py
+++ tiles/Chesttile.py
@@ -1,5 +1,6 @@
 from tiles.tile import Tile
-
+from components.inventory import Inventory
+from components.position import Position
 
 class ChestTile(Tile):
     def __init__(self):
@@ -12,4 +13,14 @@ class ChestTile(Tile):
         return False
 
     def on_interact(self, state, entity):
-        pass
+        inventory = entity.obtain_component(Inventory)
+        if not self.open:
+            if self.money:
+                inventory.ajoute_argent(self.money)
+            for id in self.items:
+                inventory.add_item(entity.ensemble_items[int(id)])
+            if state.levels[entity.obtain_component(Position).level.id].map1[self.x][self.y].variant == '1060':
+                state.levels[entity.obtain_component(Position).level.id].map1[self.x][self.y].variant = '1062'
+            else:
+                state.levels[entity.obtain_component(Position).level.id].map2[self.x][self.y].variant = '1062'
+            self.open = True
